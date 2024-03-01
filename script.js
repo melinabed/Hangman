b76dba76f6e4fe19b807a649c7e9583c9d2b58ed
melinabed <melo36049@hotmail.com>
2024-03-01 13:32:54 -1000

    added unique svgs for different phases of game

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test");
+const menuIcon = document.querySelector("#menu");
+menuIcon.addEventListener("click", changeMenuIcon);
+
+let isFirstIcon = true;
+
+function changeMenuIcon() {
+  if (isFirstIcon) {
+    menuIcon.src = "x.svg";
+  } else {
+    menuIcon.src = "menu.svg";
+  }
+  isFirstIcon = !isFirstIcon;
+}
